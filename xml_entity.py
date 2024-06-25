85b58b55787978b74e43b21deaf8587ccb27085f
Alexey Iskovskikh <iskovskikh@gmail.com>
2024-06-25 16:38:25 +0300

    upd

--- xml_entity.py
+++ xml_entity.py
@@ -1,16 +1,16 @@
 import json
 from dataclasses import dataclass, field
+
 from dacite import from_dict
-from typing import Optional
 
 
 @dataclass
 class Entity:
-    title: str                               # название тега
-    comment: str = ""                        # комментарий, идущий перед тегом
-    attributes: dict = field(default=dict)   # аттрибуты тега
-    value: Optional[str]= None                     # текст тега
-    child: list["Entity"] = field(default=list)
+    title: str  # название тега
+    comment: str = ""  # комментарий, идущий перед тегом
+    attributes: dict = field(default=dict)  # аттрибуты тега
+    value: str | None = None  # текст тега
+    child: list['Entity'] = field(default=list)
 
     def to_string(self) -> str:
         result = ""
@@ -27,7 +27,6 @@ def read_json_test() -> Entity:
         json_data = json.load(f)
         print(type(json_data))
         print(f'>>>{json_data}')
-
 
     # c1 = PatientIdentityDocV.from_json(json.dumps(json_data))
     c1 = from_dict(data_class=Entity, data=json_data)
